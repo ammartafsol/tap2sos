e0114f2b8a928500445b519113d7e71a777540e3
tanzeela-fe <tanzeela.fe.tafsol@gmail.com>
2025-10-21 17:05:35 +0500

    cms remove

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,8 @@
 export const config = {
   // apiBaseUrl: "https://r1g35c2r-3026.inc1.devtunnels.ms",
-  apiBaseUrl: "https://tap2sos-backend-dd-8bd70d25feb3.herokuapp.com", // LIVE
+  // apiBaseUrl: "https://tap2sos-backend-dd-8bd70d25feb3.herokuapp.com", // LIVE
   // apiBaseUrl: "https://tap2sos-backend-b11-01b2e85e565c.herokuapp.com", // LIVE
+  apiBaseUrl: "https://tap2sos-backend-dg-302f66829072.herokuapp.com", // live
   // apiBaseUrl: "https://wb6bv6gg-3026.inc1.devtunnels.ms", // faisal tunnel
   awsBaseUrl: "https://after5-development.s3.eu-north-1.amazonaws.com",
   stripePublicKey: "",
