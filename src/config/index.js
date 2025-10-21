b97ed097d5860863893276e813fa5445a658fc5f
ammartafsol <ammar.js@tafsol.com>
2025-10-21 18:27:54 +0500

    1

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   // apiBaseUrl: "https://r1g35c2r-3026.inc1.devtunnels.ms",
   // apiBaseUrl: "https://tap2sos-backend-dd-8bd70d25feb3.herokuapp.com", // LIVE
-  apiBaseUrl: "https://tap2sos-backend-dg-302f66829072.herokuapp.com", // LIVE
+  apiBaseUrl: "https://tap2sos-backend-dg-302f66829072.herokuapp.com", // LIVE 1
   awsBaseUrl: "https://after5-development.s3.eu-north-1.amazonaws.com",
   stripePublicKey: "",
 };
